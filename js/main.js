84e3f7c893c680c12d01713857e9b4aa5766a518
OnlyDropped <asheburenkov@yandex.ru>
2021-11-15 18:58:07 +0300

    added section sale and about

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@
     autoplay: true,
     autoplaySpeed: 8000,
   });
+  $('.sale-slider').slick({
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 8000,
+  });
