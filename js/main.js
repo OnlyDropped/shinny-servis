daf5c1fc18c8c8126917f91dce346680915db800
OnlyDropped <asheburenkov@yandex.ru>
2021-11-16 23:29:29 +0300

    added section disky

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
   $('.slider').slick({
+    cssEase: 'linear',
     arrows: false,
     vertical: true,
     verticalSwiping: true,
@@ -6,9 +7,18 @@
     autoplaySpeed: 8000,
   });
   $('.sale-slider').slick({
+    cssEase: 'linear',
     arrows: false,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 8000,
+  });
+$(".star").rateYo({
+    rating: 4.6,
+    spacing: "4px",
+    readOnly: true,
+    starWidth: "16px",
+    normalFill: "#001",
+    ratedFill: "#E5E5E5"
   });
