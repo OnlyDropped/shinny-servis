075b5076de454d4c57e2ac57ffafac320810bfb2
OnlyDropped <asheburenkov@yandex.ru>
2021-11-17 18:26:25 +0300

    added section shiny

--- js/main.js
+++ js/main.js
@@ -116,3 +116,15 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+
+
+
+
+	$('.tabs-wrapper').each(function() {
+	let ths = $(this);
+	ths.find('.tab-item').not(':first').hide();
+	ths.find('.tab').click(function() {
+		ths.find('.tab').removeClass('active').eq($(this).index()).addClass('active');
+		ths.find('.tab-item').hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass('active');
+});
